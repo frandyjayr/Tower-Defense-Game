3d0f0dd2fa46caa2cd9e4aea3d6ca66fbd54df28
unknown <rbrempelis@gmail.com>
2018-10-22 18:00:27 -0700

    Fixed Easy Map Loading

---
+++ data/map/map_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Map Tiles 1" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="../img/map_tiles.png" width="128" height="160"/>
+</tileset>
--- js/screens/play.js
+++ js/screens/play.js
@@ -3,12 +3,12 @@ game.PlayScreen = me.ScreenObject.extend({
      *  action to perform on state change
      */
     onResetEvent: function() {
-		me.levelDirector.loadLevel("sample");
+		me.levelDirector.loadLevel("easy_map");
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 		me.game.world.addChild(me.pool.pull("wavemanagereasy", 608, 32), 1);
 		//me.game.world.addChild(me.pool.pull("enemy", 608, 32), 1);
-		me.game.world.addChild(me.pool.pull("end", 0, 416), 2);
+		me.game.world.addChild(me.pool.pull("end", 288, 448), 2);
 
 		me.game.world.addChild(me.pool.pull("buttonTowerAir", game.data.utilityScreenStartPosX + 64, game.data.utilityScreenStartPosY + 64), 4);
 	
