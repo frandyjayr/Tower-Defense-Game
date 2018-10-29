be407083e3f55fd833207dfc555a28783790495b
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-10-28 20:03:30 -0700

    Changed the package.json file

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var path = require("path");
 app.use(express.static('/js'));
 app.listen(port);
+
+app.get('/', function(req,res){
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
