25f4c3f8e119b52353b6231cb4fc6eea68652038
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-10-28 19:43:46 -0700

    Altered the server.js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + ‘/public’));
+app.use(express.static('/js'));
 app.listen(port);
