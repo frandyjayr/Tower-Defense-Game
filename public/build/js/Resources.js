a70902d007f572cc02ff2af044ebbae04fe6055c
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-10-29 17:00:23 -0700

    Changed the .gitignore

---
+++ public/build/js/Resources.js
@@ -0,0 +1,142 @@
+game.resources = [
+    {
+        "name": "mainmenu",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "buttonTowerAir",
+        "type": "image",
+        "src": "data/img/buttonTowerAir.png"
+    },
+    {
+        "name": "buttonTowerEarth",
+        "type": "image",
+        "src": "data/img/buttonTowerEarth.png"
+    },
+    {
+        "name": "buttonTowerFire_old",
+        "type": "image",
+        "src": "data/img/buttonTowerFire_old.png"
+    },
+    {
+        "name": "buttonTowerFire",
+        "type": "image",
+        "src": "data/img/buttonTowerFire.png"
+    },
+    {
+        "name": "buttonTowerWater_old",
+        "type": "image",
+        "src": "data/img/buttonTowerWater_old.png"
+    },
+    {
+        "name": "buttonTowerWater",
+        "type": "image",
+        "src": "data/img/buttonTowerWater.png"
+    },
+    {
+        "name": "enemyAir_old",
+        "type": "image",
+        "src": "data/img/enemyAir_old.png"
+    },
+    {
+        "name": "enemyAir",
+        "type": "image",
+        "src": "data/img/enemyAir.png"
+    },
+    {
+        "name": "enemyEarth",
+        "type": "image",
+        "src": "data/img/enemyEarth.png"
+    },
+    {
+        "name": "enemyFire",
+        "type": "image",
+        "src": "data/img/enemyFire.png"
+    },
+    {
+        "name": "enemyWater",
+        "type": "image",
+        "src": "data/img/enemyWater.png"
+    },
+    {
+        "name": "map_tiles",
+        "type": "image",
+        "src": "data/img/map_tiles.png"
+    },
+    {
+        "name": "ships",
+        "type": "image",
+        "src": "data/img/ships.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/title_screen.png"
+    },
+    {
+        "name": "towerAir",
+        "type": "image",
+        "src": "data/img/towerAir.png"
+    },
+    {
+        "name": "towerDefense_tilesheet",
+        "type": "image",
+        "src": "data/img/towerDefense_tilesheet.png"
+    },
+    {
+        "name": "towerEarth",
+        "type": "image",
+        "src": "data/img/towerEarth.png"
+    },
+    {
+        "name": "towerFire_old",
+        "type": "image",
+        "src": "data/img/towerFire_old.png"
+    },
+    {
+        "name": "towerFire",
+        "type": "image",
+        "src": "data/img/towerFire.png"
+    },
+    {
+        "name": "towerWater_old",
+        "type": "image",
+        "src": "data/img/towerWater_old.png"
+    },
+    {
+        "name": "towerWater",
+        "type": "image",
+        "src": "data/img/towerWater.png"
+    },
+    {
+        "name": "easy_map",
+        "type": "tmx",
+        "src": "data/map/easy_map.tmx"
+    },
+    {
+        "name": "sample",
+        "type": "tmx",
+        "src": "data/map/sample.tmx"
+    },
+    {
+        "name": "map_tiles",
+        "type": "tsx",
+        "src": "data/map/map_tiles.tsx"
+    },
+    {
+        "name": "towerDefense_tilesheet",
+        "type": "tsx",
+        "src": "data/map/towerDefense_tilesheet.tsx"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    }
+];
